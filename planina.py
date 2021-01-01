af5713f1ac65d555ad1675fcbb25c41b980a682a
Cyrus Tabatabai-Yazdi <24766642+cyrustabatab@users.noreply.github.com>
2020-12-31 20:24:38 -0800

    finished planina

---
+++ planina.py
@@ -0,0 +1,26 @@
+
+
+
+
+def planina():
+
+
+    n = int(input())
+
+
+
+    per_side = 2
+
+    for i in range(n):
+        per_side  = per_side * 2 - 1
+
+
+    result = per_side**2
+
+    print(result)
+
+
+
+
+planina()
+
